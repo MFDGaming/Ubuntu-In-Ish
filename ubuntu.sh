750f4c22563bacd36ee7e36431f279a8297039e9
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2019-06-23 21:32:48 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apk add wget bash && wget 
+apk add wget bash && wget http://cdimage.ubuntu.com/ubuntu-base/releases/19.04/release/ubuntu-base-19.04-base-i386.tar.gz -O rootfs.tar.gz && mkdir rootfs && tar -C rootfs/ -zxvf rootfs.tar.gz && rm rootfs.tar.gz && echo "nameserver 8.8.8.8" > rootfs/etc/resolv.conf && echo "You can start ubuntu with ./start.sh
